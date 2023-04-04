ef354612fc742b4c5bfec4f61a6350ac5d50e59f
Marat376 <khakovmarat376@gmail.com>
2023-04-04 22:50:37 +0500

    Task 4

---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/controller/LinkResponse.java
@@ -0,0 +1,6 @@
+package ru.tinkoff.edu.java.scrapper.model.controller;
+
+import java.net.URI;
+
+public record LinkResponse(Long id, URI link) {
+}
---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/controller/ListLinksResponse.java
@@ -0,0 +1,6 @@
+package ru.tinkoff.edu.java.scrapper.model.controller;
+
+import java.util.List;
+
+public record ListLinksResponse(List<LinkResponse> links, Integer size) {
+}
